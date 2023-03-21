c71d774d89a49fef3788c63205bbaf11f6adf131
Pavlos <pavlos.papakonstantopoulos@philips.com>
2023-03-21 21:49:35 +0100

    Model bags for multiple estimators. Still need to re-check the logisti regression iteration limits, but works

--- Models.py
+++ Models.py
@@ -2,37 +2,53 @@ from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 import numpy as np
 import pandas as pd
 from ETL import ETL
+from interpret.glassbox import ExplainableBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+
 
 class Models:
 
     def __init__(self):
-        self.rf = []
-        self.acc = []
-        self.feat_importance_df = pd.DataFrame(columns=['feature', 'importance'])
+        self.lgr = LogisticRegression()
+        self.rf = RandomForestClassifier()
+        self.ebm = ExplainableBoostingClassifier()
+        self.gbc = GradientBoostingClassifier()
+        self.init_models = [self.lgr, self.rf, self.gbc, self.ebm]
+        self.model_bag_df = pd.DataFrame(columns=['trained_models', 'accuracy'])
 
+    def model_bag(self, X_train, y_train, X_test, y_test):
+        # Create a bag with trained estimators and their reported accuracy
 
-    def train_forest(self, X_train, y_train, n_estimators, max_features='sqrt'):
-        self.rf = RandomForestClassifier(n_estimators, max_features=max_features)
-        self.rf.fit(X_train, y_train)
+        test_acc = []
+        trained_models = []
+        for model in self.init_models:
+            trained_model = self.train_model(model, X_train, y_train)
+            accuracy = self.test_model(model, X_test, y_test)
+            trained_models.append(trained_model)
+            test_acc.append(accuracy)
+        self.model_bag_df['trained_models'] = trained_models
+        self.model_bag_df['accuracy'] = test_acc
 
-    def test_forest(self, X_test, y_test):
-        y_pred = self.rf.predict(X_test)
-        self.acc = np.sum(y_pred == y_test)/len(y_pred == y_test)
+    def train_model(self, estimator, X_train, y_train):
+        # Given an estimator, train on the train set
 
+        estimator.fit(X_train, y_train)
 
-    def rank_feature_importance(self, feature_names):
-        self.feat_importance_df['feature'] = feature_names
-        self.feat_importance_df['importance'] = self.rf.feature_importances_
-        self.feat_importance_df.sort_values(by='importance', ascending=True, inplace=True)
+        return estimator
+
+    def test_model(self, estimator, X_test, y_test):
+        # Given a trained estimator, make prediction and check accuracy on test set
+
+        y_pred = estimator.predict(X_test)
+        acc = np.sum(y_pred == y_test) / np.size(y_pred == y_test)
+
+        return acc
+
 
 if __name__ == "__main__":
     etl = ETL(path='./Data/titanic/train.csv', list_cat=['Sex', 'Embarked'],
-                       list_num=['Age', 'SibSp', 'Pclass', 'Parch', 'Fare'], target_label='Survived')
+              list_num=['Age', 'SibSp', 'Pclass', 'Parch', 'Fare'], target_label='Survived')
     etl.pipeline()
     models = Models()
-    models.train_forest(etl.X_train, etl.y_train, n_estimators=200, max_features='sqrt')
-    models.test_forest(etl.X_test, etl.y_test)
-    models.rank_feature_importance(etl.feature_names)
-    print(models.acc)
-
-
+    models.model_bag(etl.X_train, etl.y_train, etl.X_test, etl.y_test)
+    print("Accuracy per model: ", models.model_bag_df)
